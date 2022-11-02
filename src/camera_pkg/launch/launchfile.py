74905c9dba01310fdb7f06926fee46ca29303fa4
Athi-K <94795424+Athi-K@users.noreply.github.com>
2022-11-02 17:32:02 +0100

    Setup
    added scripts from first assignment

---
+++ src/camera_pkg/launch/launchfile.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    camera_node = Node(
+        package="camera_pkg",
+        executable="camera_node"
+    )
+    image_processing_node = Node(
+        package="camera_pkg",
+        executable="image_processing_node"
+    )
+    gui_node = Node(
+        package="camera_pkg",
+        executable="gui_node"
+    )
+
+    ld.add_action(camera_node)
+    ld.add_action(image_processing_node)
+    ld.add_action(gui_node)
+    return ld
---
+++ src/camera_pkg/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'camera_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join("share", "camera_pkg"), glob("launch/launchfile.py"))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ubuntu',
+    maintainer_email='denis.meral@rub.de',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "camera_node = camera_pkg.camera_node:main",
+            "image_processing_node = camera_pkg.image_processing_node:main",
+            "gui_node = camera_pkg.gui_node:main"
+        ]
+    },
+)
