3e14fa07a2238016b10b7e5864aab3ccb449a3f3
Ayman Soultana <61703445+Hydropic@users.noreply.github.com>
2022-11-23 17:46:51 +0100

    Added Detection Package

---
+++ src/detection_package/detection_package/detection_node.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from yolov5.models import common
+
+
+class DetectionNode(Node):
+    def __init__(self):
+        super().__init__("detection")
+        self.subscriber_ = self.create_subscription(Image, "raw_image", self.callback_raw_image, 10)
+        self.publisher_ = self.create_publisher(Image, "detected_image", 10)
+        self.weights = 'yolov5s.pt' #Placeholder
+        self.data = 'data/labels.yaml' # Placeholder
+        self.device = 'cpu'
+        self.dnn = False
+        self.half = False
+        self.model = common.DetectMultiBackend(weights=self.weights,device=self.device,dnn=self.dnn,data=self.data,fp16=self.half)
+        self.stride = self.model.stride
+        self.names = self.model.names
+        self.pt = self.model.pt
+
+    def callback_raw_image(self, msg):
+        self.get_logger().info("Image to detect received")
+        raw_image = self.cv_bridge_.imgmsg_to_cv2(msg)
+        pred = self.model(raw_image)
+        pred = self.cv_bridge_.cv2_to_imgmsg(pred.render())
+        self.publisher_.publish(pred)
+        self.get_logger().info("Published detected image.")
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = DetectionNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/detection_package/detection_package/image_processing_node.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image, CompressedImage
+import cv2
+from cv_bridge import CvBridge
+
+
+class ImageProcessingNode(Node):
+    def __init__(self):
+        super().__init__("image_processor")
+
+        self.cv_bridge_ = CvBridge()
+
+        self.subscriber_ = self.create_subscription(Image, "detected_image", self.callback_raw_image, 10)
+        self.publisher_ = self.create_publisher(Image, "processed_image", 10)
+        self.get_logger().info("Image Processor started.")
+
+    def callback_raw_image(self, msg):
+        self.get_logger().info("Received raw image.")
+        raw_image = self.cv_bridge_.imgmsg_to_cv2(msg)
+
+        # Process image
+        processed_image = self.cv_bridge_.cv2_to_imgmsg(raw_image)
+
+        self.publisher_.publish(processed_image)
+        self.get_logger().info("Published processed image.")
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = ImageProcessingNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
