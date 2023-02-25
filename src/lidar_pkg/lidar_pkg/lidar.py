752a923e73913c8bf768f11db01412b0cb064f2a
JulianW23 <jwillingmann@web.de>
2023-02-25 18:14:57 +0100

    first working move_controller

--- src/lidar_pkg/lidar_pkg/lidar.py
+++ src/lidar_pkg/lidar_pkg/lidar.py
@@ -7,18 +7,18 @@ from rclpy.qos import qos_profile_sensor_data
 import message_filters
 
 
-
 def lin_map(x):
-    #return 1/(212 - 148) * (x-148)
-    if x <30:
-        return 1-(1/62*x)
+    # return 1/(212 - 148) * (x-148)
+    if x < 30:
+        return 1 - (1 / 62 * x)
     else:
-        return 1-(1/56*x)
+        return 1 - (1 / 56 * x)
+
 
 # 48,5 25,8 6,3
 def filter_scan(scan: LaserScan):
     for r in range(len(scan.ranges)):
-        #if r >= 1 and scan.ranges[r] != 0 and scan.ranges[r-1] == 0 and scan.ranges[r+1] == 0:
+        # if r >= 1 and scan.ranges[r] != 0 and scan.ranges[r-1] == 0 and scan.ranges[r+1] == 0:
         #    scan.ranges[r] = 0
         if scan.ranges[r] > 1:
             scan.ranges[r] = 0
@@ -34,18 +34,18 @@ def cluster(scan: LaserScan):
 
     for angle, distance in enumerate(fov):
 
-        if abs(distance - last) > (0.5) and distance != 0:
+        if abs(distance - last) > distance*0.1 and distance != 0:
             clusters.append((angle, angle, distance))
             last = distance
             index += 1
         elif distance != 0 and abs(distance - last) <= (distance * error):
             start, end, dist = clusters[index]
-            clusters[index] = (start, angle, dist+distance)
+            clusters[index] = (start, angle, dist + distance)
             last = distance
 
-    for x in range( len(clusters)):
-        start,end,dist = clusters[x]
-        clusters[x] = (start,end,(dist/((end-start)+1)))
+    for x in range(len(clusters)):
+        start, end, dist = clusters[x]
+        clusters[x] = (start, end, (dist / ((end - start) + 1)))
     return clusters
 
 
@@ -58,14 +58,14 @@ class LidarFusion(Node):
         self.lidar_scan = None
         self.detected_cones = []
 
-        self.lidar_sub=message_filters.Subscriber(self,LaserScan,"scan", qos_profile=qos_profile_sensor_data)
-        self.image_sub=message_filters.Subscriber(self,FloatArray,"bounding_box")
-        ts = message_filters.ApproximateTimeSynchronizer([self.lidar_sub,self.image_sub],50,0.1,False)
+        self.lidar_sub = message_filters.Subscriber(self, LaserScan, "scan", qos_profile=qos_profile_sensor_data)
+        self.image_sub = message_filters.Subscriber(self, FloatArray, "bounding_box")
+        ts = message_filters.ApproximateTimeSynchronizer([self.lidar_sub, self.image_sub], 50, 0.1, False)
         ts.registerCallback(self.bounding_callback)
-        #self.create_subscription(LaserScan, "scan", self.lidar_callback, qos_profile_sensor_data)
-        #self.create_subscription(FloatArray, "bounding_box", self.bounding_callback, 10)
-        self.publisher = self.create_publisher(FloatArray, "lidar_values",10)
-        #self.create_timer(10,self.bounding_callback)
+        # self.create_subscription(LaserScan, "scan", self.lidar_callback, qos_profile_sensor_data)
+        # self.create_subscription(FloatArray, "bounding_box", self.bounding_callback, 10)
+        self.publisher = self.create_publisher(FloatArray, "lidar_values", 10)
+        # self.create_timer(10,self.bounding_callback)
 
     """"
     def lidar_callback(self, msg):
@@ -78,13 +78,12 @@ class LidarFusion(Node):
 
        # self.get_logger().info(f'angle {str(msg.ranges.index(range))} ranges   {str(range)}')
     """
-    def bounding_callback(self,lidar,msg):
+
+    def bounding_callback(self, lidar, msg):
         self.lidar_scan = lidar
 
-
-        #filter_scan(self.lidar_scan)
+        # filter_scan(self.lidar_scan)
         self.lidar_scan = cluster(self.lidar_scan)
-
 
         processed_bounding_box = []
         for lst in msg.lists:
@@ -93,22 +92,22 @@ class LidarFusion(Node):
                 box.append(e)
                 processed_bounding_box.append(box)
         fused_one = []
-        fused=[]
+        fused = []
 
         for b in processed_bounding_box:
             if b[3] > 0.15:
                 for obj in self.lidar_scan:
                     angle_s, angle_e, dist, = obj
                     possible_box = lin_map((angle_s + angle_e) / 2)
-                    #self.get_logger().info(str(possible_box) + " " + str(dist))
+                    # self.get_logger().info(str(possible_box) + " " + str(dist))
 
                     if abs(possible_box - b[0]) < 0.04:
                         if ((angle_s + angle_e) / 2, dist, b[5]) not in fused_one:
-                            if (len(fused_one) == 0):
+                            if len(fused_one) == 0:
                                 fused_one.append(((angle_s + angle_e) / 2, dist, b[5]))
-                            elif (dist < fused_one[0][1]):
+                            elif dist < fused_one[0][1]:
                                 fused_one[0] = ((angle_s + angle_e) / 2, dist, b[5])
-                if (len(fused_one) == 1 and not (fused_one[0] in fused)):
+                if len(fused_one) == 1 and not (fused_one[0] in fused):
                     fused.append(fused_one[0])
         """
         for obj in self.lidar_scan:
@@ -134,19 +133,19 @@ class LidarFusion(Node):
         self.get_logger().info(str(fused))
 
         msg_lid = FloatArray()
-        msg_lid.header.stamp=msg.header.stamp
+        msg_lid.header.stamp = msg.header.stamp
         for ele in fused:
             lid_data = FloatList()
             lid_data.elements = ele
             msg_lid.lists.append(lid_data)
 
         self.publisher.publish(msg_lid)
-              #  if possible_angle-1.0 <= scan.ang <= possible_angle+1.0:
-              #      ranges.update({scan.ang:scan.range})
-              #      if scan.range < lowest:
-               #         lowest=scan.range
-            #angle = {i for i in ranges if ranges[i] == lowest}
-            #self.get_logger().info(f'found boxes: {box[7]} at angle{angle}')
+        #  if possible_angle-1.0 <= scan.ang <= possible_angle+1.0:
+        #      ranges.update({scan.ang:scan.range})
+        #      if scan.range < lowest:
+        #         lowest=scan.range
+        # angle = {i for i in ranges if ranges[i] == lowest}
+        # self.get_logger().info(f'found boxes: {box[7]} at angle{angle}')
 
 
 def main(args=None):
@@ -159,6 +158,3 @@ def main(args=None):
 
 if __name__ == "__main__":
     main()
-
-
-
--- src/movement/movement/movement_controller.py
+++ src/movement/movement/movement_controller.py
@@ -1,3 +1,5 @@
+import math
+
 import rclpy
 from rclpy.node import Node
 from nav_msgs.msg import Odometry
@@ -7,6 +9,8 @@ import numpy
 from math import atan2, copysign, radians, cos, sin, sqrt
 import tf_transformations
 from pynput.keyboard import Key, Listener
+from geometry_msgs.msg import TransformStamped
+
 
 
 class MovementController(Node):
@@ -15,57 +19,73 @@ class MovementController(Node):
         super().__init__("controller")
 
         self.target = None
-        self.path=None
-        self.twist = self.create_publisher(Twist,"/cmd_vel",10)
+        self.path = None
+        self.twist = self.create_publisher(Twist, "/cmd_vel", 10)
         # might want to change to slam_odom
-        self.create_subscription(Odometry, "/odom", self.odom_callback, 10)
+        self.subscriber_pose = self.create_subscription(TransformStamped, "/turtlebot_pose", self.odom_callback, 10)
         # wherever we get the point list
-       # self.create_subscription(None,"None",self.path_callback,10)
+        # self.create_subscription(None,"None",self.path_callback,10)
         self.subscriber_map = self.create_subscription(PointArray, "updated_points", self.map_callback, 10)
 
         listener = Listener(
-                on_press=self.on_press,
-                )
+            on_press=self.on_press,
+        )
         listener.start()
 
     def on_press(self, key):
-        if key.char == "q":
-            stop = Twist()
-            self.twist.publish(Twist)
-            rclpy.shutdown()
+        try:
+            if key.char == "p":
+                stop = Twist()
+                self.twist.publish(stop)
+                rclpy.shutdown()
+        except AttributeError:
+            pass
 
-
-    def path_callback(self,msg):
+    def path_callback(self, msg):
 
         self.path = msg.data
         self.target = self.path.pop()
 
-    def odom_callback(self,msg):
+    def odom_callback(self, msg):
+        self.get_logger().info("dddd")
         if self.target is not None:
-            # calcualte distance to target
-            distance = numpy.sqrt((self.target.pose.pose.position.x-msg.pose.pose.position.x)**2
-                                  +(self.target.pose.pose.position.y-msg.pose.pose.position.y)**2)
-            if distance < 1:
+            x, y = self.target
+
+            position_x = 5-msg.transform.translation.x
+            position_y = 5-msg.transform.translation.y
+            self.get_logger().info(f' position{(position_x,position_y)}')
+            # calculate distance to target
+            distance = numpy.sqrt((x - position_x) ** 2
+                                  + (y - position_y) ** 2)
+            self.get_logger().info(str(distance))
+            if distance < 0.05:
                 # reached target
-                if len(self.path) > 0:
-                    self.target = self.path.pop()
-                else:
                     self.target=None
-                    self.twist.publish(Twist())
+                    #search
+
                     # no new points
             else:
 
-                theta=tf_transformations.euler_from_quaternion([msg.pose.pose.orientaion.x,msg.pose.pose.orientaion.y,
-                                                                msg.pose.pose.orientaion.w,msg.pose.pose.orientaion.z])
-                steering_angle = atan2(self.target.pose.pose.position.y-msg.pose.pose.position.y,
-                                     self.target.pose.pose.position.x-msg.pose.pose.position.x)
+                _ , _ , theta = tf_transformations.euler_from_quaternion([ msg.transform.rotation.x, msg.transform.rotation.y,
+                                                                          msg.transform.rotation.z,msg.transform.rotation.w])
+                theta+=math.pi
+                steering_angle = atan2(y - position_y,x - position_x)
+                rad = steering_angle-theta
+                if rad > math.pi:
+                    rad -= 2. * math.pi
+                elif rad < -math.pi:
+                    rad += 2. * math.pi
 
                 move = Twist()
-                move.linear.x = 0.2*distance #might want to change constant
-                move.angular.z = steering_angle-theta
+                move.linear.x = 0.05  # might want to change constant
+                move.angular.z = sin(steering_angle-theta)
+
+                self.get_logger().info(f'distance {distance} s_angle{steering_angle}, theta{theta} rad {rad}')
+                self.get_logger().info(f' x{move.linear.x} ,z{move.angular.z}')
 
                 self.twist.publish(move)
-
+        else:
+            self.twist.publish(Twist())
     def map_callback(self, msg):
         if len(msg.data) <= 0:
             return
@@ -77,22 +97,27 @@ class MovementController(Node):
         self.calculate_next_target(turtlestate, points)
 
     def calculate_next_target(self, turtlestate, point_map):
-        distance_threshold = 1
+        distance_threshold = 10
         sign = lambda x: copysign(1, x)
-
+        # self.get_logger().info(str(point_map))
         # get front points
         yellow = []
         blue = []
         # use a 90 degree vector to decide if a point is in front of the bot or not
-        #vector goes from bot position to a point (x2,y2) in the direction of the vector
-        vect_angle = radians(turtlestate["angle"] + 90)
+        # vector goes from bot position to a point (x2,y2) in the direction of the vector
+        vect_angle = radians(turtlestate["angle"]+90)
+        self.get_logger().info(f'vec{math.degrees(vect_angle)} turt{turtlestate["angle"]}')
+
+
         x2 = cos(vect_angle) * 2
         y2 = sin(vect_angle) * 2
+        fronts = []
         for point in point_map:
-            #check if point is "left" or "right" of vector
+            # check if point is "left" or "right" of vector
             front = sign((x2 - turtlestate["x"]) * (point[1] - turtlestate["y"]) -
                          (y2 - turtlestate["y"]) * (point[0] - turtlestate["x"]))
-            if front > 0:
+            if front < 0:
+                fronts.append(point)
                 # only points with a distance < distance_threshold are valid
                 distance = sqrt((point[0] - turtlestate["x"]) ** 2 + (point[1] - turtlestate["y"]) ** 2)
                 if distance <= distance_threshold:
@@ -100,21 +125,26 @@ class MovementController(Node):
                         blue.append([point, distance])
                     if point[2] == 2:
                         yellow.append([point, distance])
-        if len(yellow) == 0:
-            return  # turn slowly
-        if len(blue) == 0:
-            return  # turn slowly in the other direction
+        # if len(yellow) == 0:
+        #   return  # turn slowly
+        # if len(blue) == 0:
+        #   return  # turn slowly in the other direction
 
+        self.get_logger().info(str(fronts))
         yellow.sort(key=lambda x: x[1])
         blue.sort(key=lambda x: x[1])
+
 
         min_yellow = yellow[0][0]
         min_blue = blue[0][0]
 
+
         middle_point = ((min_yellow[0] - min_blue[0]) * 0.5 + min_blue[0],
                         (min_yellow[1] - min_blue[1]) * 0.5 + min_blue[1])
+        self.get_logger().info(str(middle_point))
+        if self.target is None:
+            self.target = middle_point
 
-        self.target = middle_point
 
 def main(args=None):
     rclpy.init(args=args)
